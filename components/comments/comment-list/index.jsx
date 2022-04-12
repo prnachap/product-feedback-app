0ea9d34ee7f3f8fc136259abb3969aeff9882401
Nachappa <Nachappapr1991@gmail.com>
2022-04-12 21:46:48 +0530

    feature: add total comments logic

--- components/comments/comment-list/index.jsx
+++ components/comments/comment-list/index.jsx
@@ -1,18 +1,26 @@
 import React from "react";
+
 import Card from "../../../ui/card/Card";
+import Comment from "../comment";
+
+import { getTotalComments } from "../../../utils";
 
 import styles from "./CommentList.module.scss";
-import Comment from "../comment";
 
 const Comments = (props) => {
   const { comments } = props;
+
   return (
     <Card>
-      <h3 className={styles.comment}>4 comments</h3>
-      {comments &&
-        comments.map((comment) => {
-          return <Comment key={comment.id} {...comment} />;
-        })}
+      <h3 className={styles.comment}>{`${getTotalComments(
+        comments
+      )} comments`}</h3>
+      <div className={styles.commentsContainer}>
+        {comments &&
+          comments.map((comment) => {
+            return <Comment key={comment._id} {...comment} />;
+          })}
+      </div>
     </Card>
   );
 };
--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,5 @@
 export const getRemainingCharacters = (text) => {
-  let remainingCharacters = 250 - text.length;
+  let remainingCharacters = 250 - text?.length ?? 0;
   if (remainingCharacters < 0) {
     remainingCharacters = 0;
   }
@@ -13,4 +13,18 @@ export const getOptionsForDropdown = (options) => {
 export const getTokenFromLocalStorage = () => {
   const token = JSON.parse(localStorage.getItem("token"));
   return token;
+};
+
+export const getTotalComments = (data) => {
+  let count = 0;
+  data.forEach((item) => getTotal(item));
+  function getTotal(obj) {
+    if (!obj?.comments.length) {
+      return (count += 1);
+    }
+    count++;
+    return count + obj?.comments.forEach((item) => getTotal(item));
+  }
+
+  return count;
 };
